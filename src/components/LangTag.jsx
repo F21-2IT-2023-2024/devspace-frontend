fefb20971c21fe21b110f4c24b8ee01a7bf26e3e
Sandra Elise Eng <sandra.eng@live.no>
2024-04-09 11:58:07 +0200

    Update LangTag component to toggle selection on click

--- src/components/LangTag.jsx
+++ src/components/LangTag.jsx
@@ -1,6 +1,15 @@
+import React, { useState } from 'react';
+
 const LangTag = ({ text }) => {
+    const [isSelected, setIsSelected] = useState(false);
+
+    const toggleSlected = () => {
+        setIsSelected(!isSelected);
+        console.log(`${text} is clicked`); // Console log
+    }
+
     return (
-        <p className="bg-white p-4 rounded-lg">{text}</p>
+        <p onClick={toggleSlected} className={`p-4 rounded-lg bg-gray-50 mx-2 hover:cursor-pointer ${isSelected ? 'ring ring-gray-300' : ''}`} >{text}</p>
     )
 }
 
