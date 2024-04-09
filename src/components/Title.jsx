879a45617761743976678c3e9a829df0c958ee49
Sandra Elise Eng <sandra.eng@live.no>
2024-04-09 09:20:12 +0200

    Added tailwind classes to title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,10 @@
+function Title() {
+    return ( 
+        <div className="flex flex-col items-center justify-center mt-10">
+            <h1 className="text-3xl">Welcome to</h1>
+            <h1 className="font-concertOne text-5xl">&lt;DevSpace/&gt;</h1>
+        </div>
+    )
+}
+
+export default Title;
