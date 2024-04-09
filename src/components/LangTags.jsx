d5e372e91ddf0e517035d6fab6da07302a5746bc
Sandra Elise Eng <sandra.eng@live.no>
2024-04-09 11:35:18 +0200

    Added LangTags component to project

---
+++ src/components/LangTags.jsx
@@ -0,0 +1,9 @@
+const LangTags = ({children}) => {
+    return (
+        <div className="flex flex-row items-center justify-center mt-5">
+            {children}
+        </div>
+    )
+}
+
+export default LangTags;
