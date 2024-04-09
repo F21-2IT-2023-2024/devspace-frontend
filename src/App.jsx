c6fcc2c7797658f3ed1569f7ba02470965bcd349
Sandra Elise Eng <sandra.eng@live.no>
2024-04-09 11:35:30 +0200

    Added LangTags and LangTag components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css';
 import Title from './components/Title';
 import SearchBar from './components/SearchBar';
+import LangTag from './components/LangTag';
+import LangTags from './components/LangTags';
+
 
 function App() {
   return (
     <>
       <Title />
       <SearchBar />
+      <LangTags>
+        <LangTag text="JavaScript" />
+        <LangTag text="Python" />
+      </LangTags>
+      
     </>
   );
 }
