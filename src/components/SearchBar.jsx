31cacacdfdf766de6a73363a2c57b7aa88676750
Sandra Elise Eng <sandra.eng@live.no>
2024-04-09 12:58:17 +0200

    Update search bar class to className in SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 function SearchBar() {
     return ( 
-        <form class="max-w-md mx-auto mt-12">
+        <form className="max-w-md mx-auto mt-12">
             <div>
                 <svg xmlns="http://www.w3.org/2000/svg" className="relative w-6 h-6 text-gray-400 top-10 left-3" fill="none" stroke="currentColor" strokeWidth="2">
                     <circle cx="11" cy="11" r="8"></circle>
