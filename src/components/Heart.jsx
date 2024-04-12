e537a06b411d7ec37d582acdaf4dc1be9955c7f8
Sandra Elise Eng <sandra.eng@live.no>
2024-04-12 11:23:29 +0200

    Remade heart icon

--- src/components/Heart.jsx
+++ src/components/Heart.jsx
@@ -10,8 +10,8 @@ function Heart() {
 
     return (
         <>
-            <svg onClick={toggleLiked} xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-red-500 cursor-pointer" fill={isLiked ? "red " : "none"} stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 21.5l-1.45-1.32C5.4 16.29 2 13.36 2 9.5 2 7.01 3.5 5 5.5 5c1.34 0 2.61.99 3.5 2.5C9.89 5.99 11.16 5 12.5 5c2 0 3.5 2.01 3.5 4.5 0 3.86-3.4 6.79-8.55 10.68L12 21.5z"></path>
+            <svg onClick={toggleLiked} xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-red-500 cursor-pointer" fill={isLiked ? "red" : "none"} stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 20.35l-1.45-1.32C4.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41 1.06 4.5 2.72C13.09 4.06 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-2.4 6.86-8.55 10.53L12 20.35z"></path>
             </svg>
         </>
     )
