c21a2380c6452bedbfdaa49580ab1c8901d034d5
Sandra Elise Eng <sandra.eng@live.no>
2024-05-22 10:16:07 +0200

    Updated cursor styling in ProfileBtn component

--- src/components/ProfileBtn.jsx
+++ src/components/ProfileBtn.jsx
@@ -1,8 +1,8 @@
 function ProfileBtn() {
     return (
       <div className="absolute top-0 right-0">
-        <img src="placeholder-img.png" alt="test" className="w-12 h-12 absolute top-0 rounded-full right-0 m-4"/>
-        <button className="bg-primary-700 w-12 h-12 m-4 rounded-full text-xl cursor-pointer"></button>
+        <img src="placeholder-img.png" alt="test" className="w-12 h-12 absolute top-0 rounded-full right-0 m-4 cursor-pointer"/>
+        <button className="bg-primary-700 w-12 h-12 m-4 rounded-full text-xl"></button>
       </div>
     );
   }
