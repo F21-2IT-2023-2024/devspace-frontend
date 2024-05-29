888955df56eacc6d74a11d0a26e0cc83ccadb1e0
Sandra Elise Eng <sandra.eng@live.no>
2024-05-29 09:55:54 +0200

    Add AddSnippetBtn and AddSnippetModal components

--- src/components/AddSnippetBtn.jsx
+++ src/components/AddSnippetBtn.jsx
@@ -1,9 +1,19 @@
+import React, { useState } from 'react';
+import Modal from './AddSnippetModal';
 function AddSnippetBtn() {
+  const [showModal, setShowModal] = useState(false);
+  const handleClick = () => {
+    setShowModal(!showModal);
+  };
   return (
     <div>
-      <button className="bg-primary-700 w-12 h-12 outline outline-1 outline-secondary m-4 rounded text-xl">+</button>
+      <button 
+        onClick={handleClick} 
+        className="bg-primary-700 w-12 h-12 outline outline-1 outline-secondary m-4 rounded text-xl">
+        +
+      </button>
+      {showModal && <Modal onClose={handleClick} />}
     </div>
   );
 }
-
 export default AddSnippetBtn;
