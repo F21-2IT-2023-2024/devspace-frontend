8212b841ea036a742eff49dad941e89f9eaa34c6
Sandra Elise Eng <sandra.eng@live.no>
2024-05-29 11:04:02 +0200

    Made text in textcontent smaller

--- src/components/AddSnippetModal.jsx
+++ src/components/AddSnippetModal.jsx
@@ -12,7 +12,7 @@ export default function AddSnippetModal({ onClose }) {
           id="snippetContent" 
           rows="4" cols="30" 
           placeholder='Enter your snippet here...'
-          className='resize-none bg-primary-900 text-secondary p-2 rounded-sm w-full focus:outline-none placeholder:text-sm placeholder:italic'>
+          className='resize-none bg-primary-900 text-secondary p-2 rounded-sm w-full focus:outline-none text-xs placeholder:text-sm placeholder:italic'>
         </textarea>
         <button className='bg-primary-accent text-primary px-4 py-2 mt-4 rounded-sm my-4'>Post</button>
       </div>
