b5447ae16079df3a23bd70d2c549e004e4e5e497
Sandra Elise Eng <sandra.eng@live.no>
2024-05-29 11:02:35 +0200

    Added styles to textarea

--- src/components/AddSnippetModal.jsx
+++ src/components/AddSnippetModal.jsx
@@ -6,8 +6,15 @@ export default function AddSnippetModal({ onClose }) {
       <div className="p-4 bg-primary-700 w-96 h-96 relative">
         <button onClick={onClose} className='absolute top-0 right-0 px-3 text-2xl'>x</button>
         <h2 className="text-xl mb-4">Post Snippet</h2>
-        <p>Snippet:</p>
-        <textarea name="snippetField" id="snippetContent" rows="4" cols="30" className='resize-none bg-primary-700 text-primary-900 p-2 rounded-sm w-full'></textarea>
+        <h4>Snippet:</h4>
+        <textarea 
+          name="snippetField" 
+          id="snippetContent" 
+          rows="4" cols="30" 
+          placeholder='Enter your snippet here...'
+          className='resize-none bg-primary-900 text-secondary p-2 rounded-sm w-full focus:outline-none placeholder:text-sm placeholder:italic'>
+        </textarea>
+        <button className='bg-primary-accent text-primary px-4 py-2 mt-4 rounded-sm my-4'>Post</button>
       </div>
     </div>
   );
